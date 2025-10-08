3bf9ddd85a1a95bb27f235034ad14bb4a97754a0
Nataly <Nataly>
2025-10-08 10:22:38 +0300

    Добавление методов для работы с БД#

---
+++ Archive.Infrastructure/Repository/ArchiveRepository.cs
@@ -0,0 +1,47 @@
+﻿using Archive.Domain.Models;
+using Archive.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Archive.Infrastructure.Repository
+{
+    public class ArchiveRepository: IArchiveRepository
+    {
+        private readonly ArchiveDbContext _context;
+
+        public ArchiveRepository(ArchiveDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ArchivedRecord> GetByIdAsync(int id)
+        {
+            return await _context.ArchivedRecords.FirstOrDefaultAsync(x => x.Id == id);          
+        }
+
+        public async Task<List<ArchivedRecord>> GetAllAsync()
+        {
+            return await _context.ArchivedRecords.ToListAsync();
+        }
+
+        public async Task AddAsync(ArchivedRecord record)
+        {
+            await _context.ArchivedRecords.AddAsync(record);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var record = await GetByIdAsync(id);
+            if (record != null)
+            {
+                _context.ArchivedRecords.Remove(record);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
--- Archive.Infrastructure/Repository/IArchiveRepository.cs
+++ Archive.Infrastructure/Repository/IArchiveRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Archive.Domain.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace Archive.Infrastructure.Repository
 {
     public interface IArchiveRepository
     {
+        Task<ArchivedRecord> GetByIdAsync(int id);
+        Task<List<ArchivedRecord>> GetAllAsync();
+
+        Task AddAsync(ArchivedRecord record);
+        Task DeleteAsync(int id);
     }
 }
