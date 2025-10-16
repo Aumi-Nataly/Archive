311a95e6c9fe7a002ed50fc4ecd5ad360b29a5fd
Nataly <Nataly>
2025-10-16 10:13:30 +0300

    Тестовый метод для проверки получения и обработки сообщения

---
+++ Archive/Controllers/RabbitController.cs
@@ -0,0 +1,34 @@
+﻿using Archive.Application.Features.Save;
+using Archive.Domain.Events;
+using MassTransit;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Archive.Controllers
+{
+    public class RabbitController : ControllerBase
+    {
+        private readonly IPublishEndpoint _publishEndpoint;
+
+        public RabbitController(IPublishEndpoint publishEndpoint)
+        {
+            _publishEndpoint = publishEndpoint;
+        }
+
+
+        /// <summary>
+        /// Тестовый метод для проверки получения и обработки сообщения на сохранения в архив
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost(nameof(SendToArchive_Test))]
+        public async Task<IActionResult> SendToArchive_Test()
+        {
+            await _publishEndpoint.Publish(new ArchiveCreatedCommand
+            {
+                ActivityKey = "qwerty",
+                Name = "Name",
+                Reason = "Reason"
+            });
+            return Ok();
+        }
+    }
+}
